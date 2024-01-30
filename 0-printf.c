69daf4df614da35d93bb286b68937e556c814f7d
adannalyn <qvexvon@gmail.com>
2024-01-30 03:12:54 -0800

    cs1

--- 0-printf.c
+++ 0-printf.c
@@ -6,13 +6,11 @@
  */
 int _printf(const char *format, ...)
 {
-/*	unsigned int binary_arg;*/
 	va_list alx;
 	char c;
 	char *s;
 	int fd = 1;
 	int num = 0;
-/*	int d;*/
 
 	va_start(alx, format);
 	while (*format != '\0')
@@ -25,30 +23,10 @@ int _printf(const char *format, ...)
 				c =  va_arg(alx, int);
 				num += write(fd, &c, sizeof(char));
 			}
-			/*else if (*format == 'd' || *format == 'i')
-			{
-				d = va_arg(alx, int);
-				handle_d_i(1, d);
-				num++;
-				}*/
 			else if (*format == 's')
 			{
 				s = va_arg(alx, char *);
 				print_string(s, fd, &num);
-			}
-			/*else if (*format == 'b')
-			{
-				binary_arg = va_arg(alx, unsigned int);
-				print_binary(binary_arg, fd);
-				}*/
-			/*	else if (*format == 'u')
-			{
-				print_unsigned(va_arg(alx,unsigned int, write));
-				num++;
-				}*/
-			else if (*format == '%')
-			{
-				num += write(fd, "%", sizeof(char));
 			}
 		}
 		else
