a41051744a249464629b829f9dc91ea494840bcf
adannalyn <qvexvon@gmail.com>
2024-01-30 20:27:13 -0800

    specifier s3

--- digit.c
+++ digit.c
@@ -8,27 +8,37 @@
 void handle_d_i(int fd, int nume)
 {
 	int i = 0;
-	char str[12];
+	int count;
+	char *str;
 
 	if (nume ==  0)
 	{
-		write(fd, "0", 1);
-		return;
+		count = 1;
 	}
 
+	str = (char *)malloc((count + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return;
+	}
 	if (nume < 0)
 	{
-		write(fd, "-", 1);
+		str[0] = '-';
 		nume = -nume;
+		i = 1;
 	}
 	do {
 		str[i++] = nume % 10 + '0';
 		nume /= 10;
 	} while (nume != 0);
 
+	str[i] = '\0';
+
 	while (i > 0)
 	{
 		write(fd, &str[--i], 1);
 	}
+
+	free(str);
 
 }
--- main.h
+++ main.h
@@ -1,12 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
-void handle_d_i(int fd, int nume);
-void print_string(const char *s, int fd, int *num);
 void print_binary(unsigned int n, int fd);
+void print_string(const char *s, int fd, int *num);
+void handle_d_i(int fd, int nume);
+
 
 
 
